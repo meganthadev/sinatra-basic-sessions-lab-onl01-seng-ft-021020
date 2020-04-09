e6ec5b9a005f720aedcba0deb2c41f52c28b51f0
Megan Horsfield <meganthadev@gmail.com>
2020-04-09 23:28:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  configure do 
+    enable :sessions 
+    set :session_secret, "pword"
+  end 
+  
+  get '/' 
+    erb :index 
+  end 
+
+
 end
