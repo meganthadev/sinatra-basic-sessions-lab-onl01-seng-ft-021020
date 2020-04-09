fa86272cc6cd1a09e253c922cc0964da7da5beae
Megan Horsfield <meganthadev@gmail.com>
2020-04-09 23:45:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,19 +2,18 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  configure do 
-    enable :sessions 
-    set :session_secret, "pword"
-  end 
-  
-  get '/' 
-    erb :index 
-  end 
-  
+  configure do
+    enable :sessions
+    set :session_secret, "secret"
+  end
+
+  get '/' do
+    erb :index
+  end
+
   post '/checkout' do
     @sessions = session
     item = params["item"]
     @sessions[:item] = item
   end
-
-end     
+end
